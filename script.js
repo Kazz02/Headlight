cfb41d71d042eb391d2591c585fa2b2b0a4cec80
Kazz02 <jesse.partonen@edu.turkuamk.fi>
2025-11-25 18:40:44 +0200

    updated js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const light = document.getElementById('headlight');
 
-document.addEventListener('mousemove', (e) => {
-    light.style.left = e.clientX + 'px'; // horizontal position
-    light.style.top = e.clientY + 'px'; // vertical position
+// current position of the light
+let currentX = 0;
+let currentY = 0;
+
+// target position of the light 
+let mouseX = 0;
+let mouseY = 0;
+
+document.addeEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 });
+
+function animate() {
+    // Move the current position 10% of the way towards the target position
+    currentX += (mouseX - currentX) * 0.1;
+    currentY += (mouseY - currentY) * 0.1;
+
+    light.style.left = currentX + 'px';
+    light.style.top = currentY + 'px';
+
+    requestAnimationFrame(animate); // repeats the animation on the next frame
+}
+
+animate();
